202dac86040e6fd2443ffa2af77495fef3da1862
Tristan Jones <tristankylerjones@gmail.com>
2020-10-14 20:16:10 -0500

    Ai Pathing using A*

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
-
+    public AIPath aiPath;
     public int health;
     public GameObject deathEffect;
 
     void Update()
     {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
         if (health <=0)
         {
             Instantiate(deathEffect, transform.position, Quaternion.identity);
