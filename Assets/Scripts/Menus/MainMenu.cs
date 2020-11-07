b38fc297057586f60e2aedc19905a5e3e63ef042
Tristan Jones <tristankylerjones@gmail.com>
2020-11-07 00:57:43 -0600

    Main Menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void playGame()
+    {
+        mainMenu.SetActive(false);
+        SceneManager.LoadScene("EntranceMain");
+    }
+
+    public void OpenSettings()
+    {
+
+        settingsMenu.SetActive(true);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+}
