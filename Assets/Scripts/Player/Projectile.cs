7b005aa3d251a03b28043e6433e262203b2ff259
Tristan Jones <tristankylerjones@gmail.com>
2020-11-09 01:49:05 -0600

    New Movement Speed Sprite + Animation, Fixed Enemy Death Particles

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -20,23 +20,38 @@ public class Projectile : MonoBehaviour
     
     void Update()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance);
-        if (hitInfo.collider != null)
-        {
-            if(hitInfo.collider.CompareTag("Enemy"))
-            {
-                Debug.Log("Enemy takes damage!");
-                hitInfo.collider.GetComponent<Enemy>().TakeDamage(currentDamage);
-                DestroyProjectile();
-            }
-            if(hitInfo.collider.CompareTag("Environment"))
-            {
-                Debug.Log("Projectile hit environment!");
-                DestroyProjectile();
-            }
-        }
+        // RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance);
+        // if (hitInfo.collider != null)
+        // {
+        //     if(hitInfo.collider.CompareTag("Enemy"))
+        //     {
+        //         Debug.Log("Enemy takes damage!");
+        //         hitInfo.collider.GetComponent<Enemy>().TakeDamage(currentDamage);
+        //         DestroyProjectile();
+        //     }
+        //     if(hitInfo.collider.CompareTag("Environment"))
+        //     {
+        //         Debug.Log("Projectile hit environment!");
+        //         DestroyProjectile();
+        //     }
+        // }
         
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("Player is colliding");
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(currentDamage);
+            DestroyProjectile();
+        }
+        if(collision.CompareTag("Environment"))
+        {
+            DestroyProjectile();
+        }
     }
 
      void DestroyProjectile()
